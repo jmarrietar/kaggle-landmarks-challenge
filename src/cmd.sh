3739457ee9c8dcab711a6b40faaf40e941e480f4
Jose Miguel Arrieta Ramos <jmarrietar@Joses-MacBook-Pro.local>
2019-11-12 20:50:45 -0500

    initial commit

---
+++ src/cmd.sh
@@ -0,0 +1,7 @@
+cd kaggle-landmarks-challenge
+
+python src/landmark-retrieval-challenge-image-downloader.py google-landmarks-dataset/train.csv images/stage_1/train
+
+python src/landmark-retrieval-challenge-image-downloader.py google-landmarks-dataset/test.csv images/stage_1/test
+
+python src/landmark-retrieval-challenge-image-downloader.py google-landmarks-dataset/index.csv images/stage_1/index
